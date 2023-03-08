65b29f8e8d4aa04538833da6358f086da3efd610
rico020202 <ricoroca2@gmail.com>
2023-03-08 16:05:46 -0600

    Agregar documentacion y ejercicio 16

--- Actividad 2.1.rkt
+++ Actividad 2.1.rkt
@@ -1,5 +1,8 @@
 #lang racket
-;; Add List sum of elements of a list
+;;La funci ́on add-list devuelve la suma de los
+;;numeros contenidos en la lista que recibe como entrada,
+;;o 0 si est́a vaćıa.
+;;(add-list '(1 2 3 4 5) 0) -> 15
 (define add-list
   (lambda (lst sum)
     (cond
@@ -7,16 +10,21 @@
       [(add-list (cdr lst) (+ sum (car lst)))])))
 (add-list '(1 2 3 4 5) 0)
 
-;; Invert pairs
+;; Recibe una lista de sublistas formadas de pares y
+;; regresa una nueva lista pero con las sublistas invertidas
+;; (invert-pairs '((a 1)(a 2)(b 1)(b 2))) -> ((1 a)(2 a)(1 b)(2 b))
 (define invert-pairs
   (lambda (lst)
   (cond
     [(null? lst)'()]
     [(cons (list (cadr (car lst)) (car (car lst)))
             (invert-pairs (cdr lst)))])))
-(invert-pairs '((1 2) (3 4) (5 6)))
+(invert-pairs '((a 1) (a 2) (a 3)))
 
-;;lis-of-symbols?
+;; Recibe una lista de elementos y regresa true
+;; si la lista contiene puros simbolos
+;; (list-of-symbols '(1 2 3 4 5)) -> #f
+;; (list-of-symbols '(a b c d e)) -> #t
 (define list-of-symbols
   (lambda (lst)
     (cond
@@ -26,7 +34,10 @@
 (list-of-symbols '(1 2 3 4 5))
 (list-of-symbols '(a b c d e))
 
-;;swapper
+;; Recibe una lista de elementos y 2 elementos (a, b)
+;; y regresa unas nueva lista donde cambia todas las
+;; instancias de a por b y viceversa
+;; (swapper a b '(a b b a a)) -> '(b a a b b)
 (define swapper
   (lambda (a b lst)
     (cond
@@ -36,10 +47,28 @@
       [else (cons (car lst) (swapper a b (cdr lst)))])))
 (swapper 'purr 'kitty '(soft kitty warm kitty little ball of fur happy kitty sleepy kitty purr purr purr))
 
-;;dot-product
+;; Recibe dos listas y regresa el producto punto de estas 2
+;; (dot-product '(1 2 3) '(4 5 6)) -> 32
 (define dot-product
   (lambda (lst lst2)
     (cond
       [(empty? (or (and lst lst2) (lst) (lst2))) 0]
       [else (+ (* (car lst) (car lst2)) (dot-product (cdr lst) (cdr lst2)))])))
-(dot-product '(1 2 3) '(2 4 3))
+(dot-product '(1 2 3) '(4 5 6))
+
+;; Recibe una lista de numeros y regresa la media artimertica
+;; (average '(1 2 3 4 5)) -> 3
+(define average
+  (lambda (lst)
+    (cond
+      [(empty? lst) 0]
+      [else (/ (add-list lst 0) (length lst))])))
+(average '(1 2 3 4 5))
+
+;;Standard deviation
+
+;;Replic
+
+;;Expand
+
+;;Binary
