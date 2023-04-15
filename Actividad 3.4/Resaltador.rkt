57feea93117628fdb63123c0c433788d00722827
rico020202 <ricoroca2@gmail.com>
2023-04-14 21:39:17 -0600

    Entrega

--- Actividad 3.4/Resaltador.rkt
+++ Actividad 3.4/Resaltador.rkt
@@ -1,6 +1,5 @@
 ;; Jose Ricardo Rosales Castañeda
 ;; Dante David Perez Perez
-
 #lang racket
 
 (require 2htdp/batch-io)
@@ -23,9 +22,9 @@
 
 ;; operator from c#
 (define operator
-  '("+" "-" "*" "/" "%" "&" "|" "^" "!" "~" "=" "<" ">" "+=" "-=" "*=" "/=" "%="
-    "&=" "|=" "^=" "<<" ">>" ">>>" "<<=" ">>=" ">>>=" "==" "!=" "<=" ">=" "&&" "||"
-    "++" "--" "?" "??"))
+  '("&=" "|=" "^=" "<<" ">>" ">>>" "<<=" ">>=" ">>>=" "==" "!=" "<=" ">=" "&&" "||"
+    "+" "-" "*" "/" "%" "&" "|" "^" "!" "~" "=" "<" ">" "+=" "-=" "*=" 
+    "/=" "%=" "++" "--" "?" "??"))
 
 ;; separator from c#
 (define separator
@@ -67,90 +66,186 @@
 )
 
 ;;Replace all tokens
-(define (replace-all-tokens s open-block-comment)
-    (define word '())
-    (define list-line '())
-    (define open-quotes #f)
-    (define possible-line-comment #f)
-    (define open-line-comment #f)
-
+(define (replace-all-tokens s multiline-comment)
+    (define string '())
+    (define string-list '())
+    (define string-identifier #f)
+    (define string-comment #f)
+    (define open-string-comment #f)
     (define ch (regexp-split #px"" s))
 
      (for/last ([char ch])
-      (when (and (eq? char (last ch)) (or open-line-comment open-block-comment))
-        (set! list-line (append list-line (list word))))
-
-      (cond 
-        [open-block-comment (set! word (append word (list char)))]
-        [(regexp-match #rx"#" char) (set! word (append word (list char)))]
-        [(regexp-match? #rx"[a-zA-Z0-9_]" char)
-         (set! word (append word (list char)))]
-
-        ; Match for s comments
-        [(regexp-match #px"/" char) 
-          (cond 
-            [possible-line-comment 
-              ((lambda () 
-                (set! possible-line-comment #f)
-                (set! open-line-comment #t)
-                (set! word (append word (list char)))))]
-            [else 
-              ((lambda () 
-                (set! possible-line-comment #t)
-                (set! word (append word (list char)))))])]
-
-        [open-line-comment (set! word (append word (list char)))]
-
-        [(regexp-match? #px"\"" char)
-         ((lambda ()
-            (set! open-quotes (not open-quotes))
-            (set! word (append word (list char)))))]
-
-        [open-quotes (set! word (append word (list char)))]
-
-        ; Match for operator
-        [(member char operator)
-         ((lambda ()
-            (set! list-line (append list-line (list word)))
-            (set! word '())
-            (set! word (append word (list char)))
-            (set! list-line (append list-line (list word)))
-            (set! word '())))]
-
-        ; Match for separator
-        [(member char separator)
-         ((lambda ()
-            (set! list-line (append list-line (list word)))
-            (set! word '())
-            (set! word (append word (list char)))
-            (set! list-line (append list-line (list word)))
-            (set! word '())))]
-
-        ; Match for any other character
-        [else
-         ((lambda ()
-            (set! list-line (append list-line (list word)))
-            (set! word '())))])
+      (when (and (eq? char (last ch)) (or open-string-comment multiline-comment)) (set! string-list (append string-list (list string))))
+        (cond 
+          [multiline-comment (set! string (append string (list char)))]
+          [(regexp-match #rx"#" char) (set! string (append string (list char)))]
+          [(regexp-match? #rx"[a-zA-Z0-9_]" char)
+          (set! string (append string (list char)))]
+          [(regexp-match #px"/" char) 
+            (cond 
+              [string-comment 
+                ((lambda () 
+                  (set! string-comment #f)
+                  (set! open-string-comment #t)
+                  (set! string (append string (list char)))))]
+              [else 
+                ((lambda () 
+                  (set! string-comment #t)
+                  (set! string (append string (list char)))))])]
+          [open-string-comment (set! string (append string (list char)))]
+          [(regexp-match? #px"\"" char)
+          ((lambda ()
+              (set! string-identifier (not string-identifier))
+              (set! string (append string (list char)))))]
+          [string-identifier (set! string (append string (list char)))]
+          [(member char operator)
+          ((lambda ()
+              (set! string-list (append string-list (list string)))
+              (set! string '())
+              (set! string (append string (list char)))
+              (set! string-list (append string-list (list string)))
+              (set! string '())))]
+          [(member char separator)
+          ((lambda ()
+              (set! string-list (append string-list (list string)))
+              (set! string '())
+              (set! string (append string (list char)))
+              (set! string-list (append string-list (list string)))
+              (set! string '())))]
+          [else
+          ((lambda ()
+              (set! string-list (append string-list (list string)))
+              (set! string '())))]
+        )
     )
 
-    (define tokens (map (lambda (x) (string-join x "")) list-line))
+    (define tokens (map (lambda (x) (string-join x "")) string-list))
     (define (categorize-tokens tokens)
-      (define (loop tokens open-block-comment)
+      (define (loop tokens multiline-comment)
         (cond
           [(null? tokens) '()]
-          [(and (string=? (car tokens) "/*") (not open-block-comment))
+          [(and (string=? (car tokens) "/*") (not multiline-comment))
             (append (list (categorize-token (car tokens))) (loop (cdr tokens) #t))]
-          [(and (string=? (car tokens) "*/") open-block-comment)
+          [(and (string=? (car tokens) "*/") multiline-comment)
             (append (list (categorize-token (car tokens))) (loop (cdr tokens) #f))]
-          [open-block-comment
+          [multiline-comment
             (append (list (replace-comment (car tokens))) (loop (cdr tokens) #t))]
           [(string=? (car tokens) "//")
             (append (list (replace-comment (car tokens))) (loop (cdr tokens) #f))]
           [else
             (append (list (categorize-token (car tokens))) (loop (cdr tokens) #f))]))
-      (loop tokens open-block-comment))
+      (loop tokens multiline-comment))
 
     (categorize-tokens tokens)
+)
+
+;; html header
+(define html-header
+  "<!DOCTYPE html>
+  <html>
+  <head>
+    <meta chet=\"utf-8\">
+    <title>Resaltador de sintaxis</title>
+    <link rel=\"stylesheet\" href=\"style.css\">
+  </head>
+  <body>
+  <pre>"
+)
+
+;; html footer
+(define html-footer
+  "</pre>
+  </body>
+  </html>"
+)
+
+;; css file
+(define css
+  "body {
+    background-color: #2b2b2b;
+    color: #f8f8f2;
+    font-family: Consolas, monospace;
+    font-size: 14px;
+    margin-left: 30px;
+  }
+
+  pre {
+    margin: 0;
+  }
+
+  span {
+    display: inline-block;
+  }
+
+  span.keyword {
+    color: #ff0000;
+  }
+
+  span.operator {
+    color: #d10000;
+    margin-right: -7px;
+    margin-left: -7px;
+  }
+
+  span.separator {
+    color: #ffff00;
+    margin-right: -7px;
+    margin-left: -7px;
+  }
+
+  span.identifier {
+    color: #cccccc;
+  }
+
+  span.number {
+    color: #bf00ff;
+  }
+
+  span.string {
+    color: #ffee58;
+  }
+
+  span.comment {
+    color: #75715e;
+  }
+
+  span.System {
+    color: #8453ff;
+  }"
+)
+
+;; function to print lines in html
+(define (print input-strings)
+  (define multiline-comment #f)
+
+  (define (process-string s)
+    (when (not multiline-comment)
+      (set! multiline-comment (regexp-match? #px"/\\*" s)))
+
+    (define tokens (replace-all-tokens s multiline-comment))
+    (define formatted-string (string-join tokens " "))
+
+    (when multiline-comment
+      (set! multiline-comment (not (regexp-match? #px"\\*/" s))))
+
+    (string-append "<pre>" formatted-string "</pre>"))
+
+  (define processed-strings (map process-string input-strings))
+  (string-join processed-strings "")
+)
+
+;; function to generate html file
+(define (generate-html input-strings)
+  (write-file "output.html" 
+    (string-append html-header (print input-strings) html-footer)
+  )
+)
+
+;; function to generate css file
+(define (generate-css)
+  (write-file "style.css" 
+    (string-append css)
+  )
 )
 
 ;; input file
@@ -159,46 +254,14 @@
 ;; output file
 (define output-file "output.html")
 
-;; html header
-(define html-header
-  "<!DOCTYPE html>
-<html>
-<head>
-  <meta chet=\"utf-8\">
-  <title>Resaltador de sintaxis</title>
-  <link rel=\"stylesheet\" href=\"style.css\">
-</head>
-<body>
-  <pre>")
-
-;; html footer
-(define html-footer
-  "</pre>
-</body>
-</html>")
+;; css file
+(define css-file "style.css")
 
 ;; Run
-(define (Run input-file output-file)
-  (define input-lines (file->lines input-file))
-  (define output-port (open-output-file output-file))
-  (write-string html-header output-port)
+(define (Run)
+  (define input-strings (file->lines input-file))
+  (generate-html input-strings)
+  (generate-css)
+)
 
-  (define open-block-comment #f)
-
-  (for-each (lambda (s)
-              (when (not open-block-comment) 
-                  (set! open-block-comment (regexp-match? #px"/\\*" s)))
-                  
-              (define tokens (replace-all-tokens s open-block-comment))
-              (define formatted-line (string-join tokens " "))
-
-              (when open-block-comment
-                  (set! open-block-comment (not (regexp-match? #px"\\*/" s))))
-
-               (write-string (string-append "<pre>" formatted-line "</pre>") output-port))
-            input-lines)
-  
-  (write-string html-footer output-port)
-  (close-output-port output-port))
-
-(time (Run input-file output-file))
+(time (Run))
